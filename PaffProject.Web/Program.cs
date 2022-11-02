015963c688c0348d06979d20460044c158efdd85
Mitchell Paff <mitchpaff@gmail.com>
2022-11-02 17:02:08 +0100

    feat: Initial commit

---
+++ PaffProject.Web/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Azure;
+using Azure.Identity;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddAzureClients(x =>
+{
+    x.AddBlobServiceClient(new Uri("https://<account-name>.blob.core.windows.net"));
+    x.UseCredential(new DefaultAzureCredential());
+});
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
