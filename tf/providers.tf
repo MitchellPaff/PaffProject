23fa46822eb37a0bc912b05623588566417dde25
Mitchell Paff <mitchpaff@gmail.com>
2022-11-23 17:03:25 +0100

    feat: hello

--- tf/providers.tf
+++ tf/providers.tf
@@ -1,22 +1,21 @@
 terraform {
   required_version = ">=0.12"
 
+  backend "azurerm" {
+    resource_group_name  = "tstate"
+    storage_account_name = "pafftfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~>3.32.0"
     }
-    random = {
-      source  = "hashicorp/random"
-      version = "~>3.0"
-    }
-    azuread = {
-      source  = "hashicorp/azuread"
-      version = "~> 2.15.0"
-    }
   }
 }
-
 provider "azurerm" {
   features {}
 }
+
